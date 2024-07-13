b66aeebda3522b25fbdd810737fe83f73ecad646
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-07-13 18:49:13 +0200

    rimosso commenti eccessivi

--- services/backend/main.go
+++ services/backend/main.go
@@ -1,46 +1,43 @@
 package main
 
 import (
-	"crypto/tls"
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"students/api/router"
+	"crypto/tls"
+	"database/sql"
+	
+	_ "github.com/lib/pq"
+	
+	TranspLayerCustom "students/tls"
 	"students/db"
 	"students/env"
-	tls_ "students/tls"
-	_ "github.com/lib/pq"
+	"students/api/router"
+	
 )
 
 func main() {
-	fmt.Println("Setting up environment variables from ./env/.env")
-	envm := env.SetupEnv("./env/.env")
+
+	envm := env.SetupEnv("env/.env")
 	fmt.Printf("Environment setup complete: %+v\n", envm)
 
-	fmt.Printf("Opening the database\n")
 	dbase, err := sql.Open(envm.DbDriver, envm.DbSource)
 	if err != nil {
 		log.Printf("failed to open the database connection: %v\n", err)
 	}
 
-	fmt.Printf("Pinging Database\n")
 	err = dbase.Ping()
 	if err != nil {
 		dbase.Close()
 		log.Printf("failed to ping the database: %v\n", err)
 	}
+	
+	slqcDatabase := db.New(dbase)
 	fmt.Printf("Database Connected\n")
 
-	fmt.Println("Initializing database connection")
-	slqcDatabase := db.New(dbase)
-	fmt.Println("Database connection initialized")
-
-	fmt.Println("Setting up TLS configuration")
-	config := tls_.NewConfig()
+	config := TranspLayerCustom.NewConfig()
 	fmt.Println("TLS configuration set up")
 
-	fmt.Println("Setting up router")
 	router := router.NewRouter(slqcDatabase)
 	fmt.Println("Router setup complete")
 
