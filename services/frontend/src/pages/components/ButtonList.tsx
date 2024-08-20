fb3e902d7f8718de2b99a975cf2f908361def266
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-08-20 18:13:57 +0200

    [button component] create button file handler

--- services/frontend/src/pages/components/ButtonList.tsx
+++ services/frontend/src/pages/components/ButtonList.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-export default function ButtonList() {
-  return (
-    <div className="flex flex-col  w-1/12 bg-white">
-      <button className="bg-emerald-600 hover:bg-emerald-700 transition duration-200 terminal-button">
-        GET LATEST
+  export default function ButtonList({ onFormChange }: { onFormChange: (value: string) => void }) { 
+    return (
+    <div className="flex flex-col bg-transparent space-y-28 w-1/12">
+      <button className="bg-emerald-600 hover:bg-emerald-700 transition  duration-200" onClick={() => onFormChange("single")}>
+        GET SINGLE MESSAGE
       </button>
-      <button className="bg-emerald-600 hover:bg-emerald-700 transition duration-200 terminal-button">
+      <button className="bg-emerald-600 hover:bg-emerald-700 transition  duration-200" onClick={() => onFormChange("all")}>
         GET ALL
       </button>
-      <button className="bg-emerald-600 hover:bg-emerald-700 transition duration-200 terminal-button">
+      <button className="bg-emerald-600 hover:bg-emerald-700 transition  duration-200" onClick={() => onFormChange("post")}>
         POST
       </button>
-      <button className="bg-emerald-600 hover:bg-emerald-700 transition duration-200 terminal-button">
+      <button className="bg-emerald-600 hover:bg-emerald-700 transition  duration-200" onClick={() => onFormChange("modify")}>
         MODIFY
       </button>
-      <button className="bg-emerald-600 hover:bg-emerald-700 transition duration-200 terminal-button">
+        <button className="bg-emerald-600 hover:bg-emerald-700 transition  duration-200" onClick={() => onFormChange("delete")}>
         DELETE
       </button>
     </div>
